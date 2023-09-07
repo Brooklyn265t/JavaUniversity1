96ac4189217daeb4d413a9053ea96978c03f3d06
Brooklyn265t <140600549+Brooklyn265t@users.noreply.github.com>
2023-09-07 20:55:28 +0300

    Add files via upload

---
+++ Java_homework1_3.java
@@ -0,0 +1,10 @@
+package ru.mirea.lab1;
+
+public class Java_homework1_3 {
+    public static void main(String[] args) {
+        for (int i = 0; i < args.length; i++) {
+            System.out.println("Аргумент= " + args[i]);
+        }
+        System.out.println("Аргумент= " + args.length);
+    }
+}
